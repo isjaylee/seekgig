a170d9e807b563d76b90eb25771fa12479896123
Jay Lee <leejay@live.com>
2016-03-18 08:33:43 -0500

    Add context to review_spec associations.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe Review do
-
-  it { should belong_to :user }
-  it { should belong_to :gig }
-  it { should validate_presence_of :user_id }
-  it { should validate_presence_of :gig_id }
-
+  context ".association & validations" do
+    it { should belong_to :user }
+    it { should belong_to :gig }
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :gig_id }
+  end
 end
