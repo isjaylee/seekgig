ff0c4b6271d31c20cc39dd051c6bea474ae2640c
Jay Lee <leejay@live.com>
2016-03-21 09:03:51 -0500

    Nokogiri in production use system libraries.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 server '159.203.164.154', user: 'deploy', roles: %w{web app}
-
+set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
